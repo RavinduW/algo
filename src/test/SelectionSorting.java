81f5747bc3852ad665f6a794339109baaf2ff9a6
RavinduW <raviduchinthaka3@gmail.com>
2019-06-14 13:23:07 +0530

    selection sort

--- src/test/SelectionSorting.java
+++ src/test/SelectionSorting.java
@@ -13,9 +13,44 @@ package test;
  */
 public class SelectionSorting {
     
-    public static void main(String args[]){
+    int[] selectionSort(int [] x){
         
+        //outer loop
+        for(int i=0;i<x.length-1;i++){
+            
+            int minIndex = i;
+            
+            //loop for find the minimum index
+            for(int j=i+1;j<x.length-1;j++){
+                if(x[minIndex]>x[j]){
+                    minIndex = j;
+                }
+            }
+            
+            //swap the values
+            int temp = x[minIndex];
+            
+            x[minIndex]= x[i];
+            
+            x[i] = temp;
+            
+            
+        }
+        return x;
+    }
+    
+    public static void main(String args[]){
+        SelectionSorting ss = new SelectionSorting();
+        int arr [] = {4,3,1,9,4,-2,-3};
+        int[] y;
+        y = ss.selectionSort(arr);
+        
+        for(int k=0;k<y.length-1;k++){
+            System.out.println(y[k]);
+        }
     }
     
     
 }
+
+//time complexity of selection sort => O(n^2) bcz it uses 2 for loops
